7dba42269e44d96d126ed34e78d3c9c16f212039
Alec Mas <alecmas@gmail.com>
2017-02-16 12:43:44 -0500

    Add files via upload

---
+++ bigDigits.py
@@ -0,0 +1,51 @@
+# file bigdigits2.py
+# prints digits with their respective numbers, instead of asterisks
+# Alexander Mas
+
+import sys
+
+Zero = ["  ***  ",
+        " *   * ",
+        "*     *",
+        "*     *",
+        "*     *",
+        " *   * ",
+        "  ***  "]
+One = [" * ", "** ", " * ", " * ", " * ", " * ", "***"]
+Two = [" *** ", "*   *", "*  * ", "  *  ", " *   ", "*    ", "*****"]
+Three = [" *** ", "*   *", "    *", "  ** ", "    *", "*   *", " *** "]
+Four = ["   *  ", "  **  ", " * *  ", "*  *  ", "******", "   *  ",
+        "   *  "]
+Five = ["*****", "*    ", "*    ", " *** ", "    *", "*   *", " *** "]
+Six = [" *** ", "*    ", "*    ", "**** ", "*   *", "*   *", " *** "]
+Seven = ["*****", "    *", "   * ", "  *  ", " *   ", "*    ", "*    "]
+Eight = [" *** ", "*   *", "*   *", " *** ", "*   *", "*   *", " *** "]
+Nine = [" ****", "*   *", "*   *", " ****", "    *", "    *", "    *"]
+
+Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+
+try:
+    digits = sys.argv[1]
+    row = 0
+    while row < 7:
+        line = ""
+        column = 0
+        while column < len(digits):
+            number = int(digits[column])
+            digit = Digits[number]
+            # set the current row and for each asterisk that is found, append
+            # that number to the line (ie. for 7, *** will append 777)
+            currentRow = digit[row]
+            for i in digit[row]:
+                if i == "*":
+                    line += str(number)
+                else:
+                    line += " "
+            line += " "
+            column += 1
+        print(line)
+        row += 1
+except IndexError:
+    print("usage: bigdigits.py <number>")
+except ValueError as err:
+    print(err, "in", digits)
